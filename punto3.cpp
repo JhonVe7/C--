4588746a623308c9edec9e402ef9971c7f3d2d0a
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2017-11-26 13:10:02 -0500

    points 3 & 4 full
    in this repository  the points 3 & 4 are ready for operation

--- punto3.cpp
+++ punto3.cpp
@@ -1,51 +1,78 @@
 #include<iostream>
-int main (){
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+
+using namespace std;
+
+string NumberToString(int pNumber)
+{
+ ostringstream oOStrStream;
+ oOStrStream << pNumber;
+ return oOStrStream.str();
+}
+
+int main(int argc, char* argv[]){
 int m = 0 ;
 int npar = 0 ;
 int nimpar = 0;
 int npos = 0;
 int nneg = 0 ;
-String cpar = "";
-String cimpar = "";
-String cpos = "";
-String cneg = "";
-Cout ("ingrese las dimenciones de la Matriz");
+
+string cpar, cimpar, cpos, cneg, x;
+cout << "ingrese las dimenciones de la Matriz: ";
 cin >> m ;
 int matriz [m][m];
-	for (int = 0 ;i<m ;i++){
-		for (int = 0 ;i<m ;i++){
-			matriz[i][j] = rand ()% 100 - 100;
+cout <<"los numeros aleatorios son: \n\n";
+
+
+	for (int i= 0 ;i<m ;i++){
+		for (int j= 0 ;j<m ;j++){
+			matriz[i][j] = rand()% 200 - 100;
 		}
 	}
+	
+	for (int i=0;i<m ;i++){
+		x = "";
+		for (int j= 0 ;j<m ;j++){
+			x = x + "[" + NumberToString(matriz[i][j])+ "]";
+		}
+		cout << x + "\n";	
+	}
+		
+	
+	
 	for(int i = 0;i<m;i++){
-		for(int j = 0; j<m;i++){
+		for(int j = 0; j<m;j++){
 			if(matriz[i][j]%2 == 0){
 				npar++;
-				cpar = cpar + matriz [i][j] + ",";
+				cpar = cpar + NumberToString(matriz[i][j]) + ",";
 			}
 			else if (matriz[i][j]%2 != 0){
 				nimpar ++ ;
-				cimpar = cimpar+ matriz[i][j] + ",";	
+				cimpar = cimpar+ NumberToString(matriz[i][j]) + ",";	
 			}
 			if(matriz[i][j]< 0 ){
 				nneg ++;
-				cneg = cneg + matriz [i][j] + ",";
+				cneg = cneg + NumberToString(matriz[i][j]) + ",";
 			}
 			else if(matriz[i][j]>= 0){
 				npos++;
-				cpos = cpos + matriz[i][j]+ ",";			
+				cpos = cpos + NumberToString(matriz[i][j]) + ",";			
 			}
 		}
 	}
-	cout <<"la cantidad de números pares encontrados son: " + npar;
-	cout <<"la cantidad de números impares encontrados son: " + nimpar;
-	cout <<"la cantidad de números negativos encontrados son: " + nneg;
-	cout <<"la cantidad de números positivos encontrados son: " + npos;
-	cout <<"los numeros pares encontrados son: "+ cpares;
-	cout <<"los numeros impares encontrados son:" + cimpar;
-	cout <<"los numeros negativos encontrados son:" + cneg;
-	cout <<"los numeros positivos encontrados son:" + cpos;
+	cout <<"\n\nla cantidad de numeros pares encontrados son: " + NumberToString(npar )+ "\n\n";
+	cout <<"la cantidad de numeros impares encontrados son: " + NumberToString(nimpar) + "\n\n";
+	cout <<"la cantidad de numeros negativos encontrados son: " + NumberToString(nneg) + "\n\n";
+	cout <<"la cantidad de numeros positivos encontrados son: " + NumberToString(npos) + "\n\n";
+	cout <<"los numeros pares encontrados son: "+ cpar + "\n\n";
+	cout <<"los numeros impares encontrados son:" + cimpar + "\n\n";
+	cout <<"los numeros negativos encontrados son:" + cneg + "\n\n";
+	cout <<"los numeros positivos encontrados son:" + cpos + "\n\n";
 	
 }
+
+
 
 
--- punto4.cpp
+++ punto4.cpp
@@ -1,21 +1,52 @@
 #include<iostream>
-int main (){
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string NumberToString(int pNumber)
+{
+ ostringstream oOStrStream;
+ oOStrStream << pNumber;
+ return oOStrStream.str();
+}
+
+int main (int argc, char* argv[]){
+
 int m = 0 ;
-Cout ("ingrese las dimenciones de la Matriz");
+cout << "ingrese las dimenciones de la Matriz: ";
 cin >> m ;
 int matriz [m][m];
 int matrizB [m][m];
-	for (i=0;i<m;i++){
-		for(j=0;j<m;j++){
-		cout>>"ingrese el dato: ";
-		cin<< matriz[i][j];
+
+	for (int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+		cout << "ingrese el dato: ";
+		cin >> matriz[i][j];
 		}
 	}
-	for (i=0;i<m;i++){
-		for(j=0;j<m;j++){
-		matrizB [i][j]=matriz[j][i];
-		cout (matrizB[i][j]);
-		LLendL;
+	string x;
+	for (int i=0;i<m ;i++){
+		x = "";
+		for (int j= 0 ;j<m ;j++){
+			x = x + "[" + NumberToString(matriz[i][j])+ "]";
 		}
+		cout << x + "\n";	
+	}
+	
+	for (int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+		matrizB [j][i]=matriz[j][i];
+		}
+	}
+	
+	cout << "\n\nEspejo: \n\n";
+	
+	for (int i=0;i<m ;i++){
+		x = "";
+		for (int j= 0 ;j<m ;j++){
+			x = x + "[" + NumberToString(matrizB[j][i])+ "]";
+		}
+		cout << x + "\n";	
 	}
 }
